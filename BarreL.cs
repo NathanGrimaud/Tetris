8edc776652d8e3a63ef33981d6318c99d927759d
haris44 <lxndr.bertrand@gmail.com>
2015-11-26 21:54:09 +0100

    Correction partiels du bug cité précédements

--- BarreL.cs
+++ BarreL.cs
@@ -17,44 +17,62 @@ namespace Tetris
 
         public override void Tourner(ref Barre barre,ref Table grille) 
         {
+            // Sauvegarde des précédents emplacements
             List<int> emplacements = new List<int>();
+            Barre b = new BarreL();            
             foreach (var emp in barre.emplacement)
             {
                 emplacements.Add(emp);
-            }           
+            }
+            b.emplacement = emplacements;
+
+            // Déménagement de la barre 
             if (position == 0)
             {
-                emplacements[0] -= 9;
-                emplacements[1] -= 9;
-                emplacements[3] += 2;
+                this.emplacement[0] -= 9;
+                this.emplacement[1] -= 9;
+                this.emplacement[3] += 2;
                 position++;
             }
             else if (position == 1)
             {
-                emplacements[0] += 2;
-                emplacements[1] += 11;
-                emplacements[3] -= 11;
+                this.emplacement[0] += 2;
+                this.emplacement[1] += 11;
+                this.emplacement[3] -= 11;
                 position++;
             }
             else if (position == 2)
             {
-                emplacements[0] += 20;
-                emplacements[1] += 9;
-                emplacements[3] -= 9;
+                this.emplacement[0] += 20;
+                this.emplacement[1] += 9;
+                this.emplacement[3] -= 9;
                 position++;
             }
             else if (position == 3)
             {
-                emplacements[0] -= 11;
-                emplacements[2] += 11;
-                emplacements[3] -= 2;
+                this.emplacement[0] -= 11;
+                this.emplacement[2] += 11;
+                this.emplacement[3] -= 2;
                 position = 0;
             }
-            Barre b = new BarreL();
-            b.emplacement = emplacements;
-            if (grille.EmplacementDispo(ref b))
+            
+            // Vérification que le déménagement est correct
+            if (grille.EmplacementDispo(ref barre))
+            {              
+                foreach (int empl in b.emplacement)
+                {
+                    grille.tableau[empl] = null;
+                }
+
+                foreach (int empl in this.emplacement)
+                {
+                    grille.tableau[empl] = this;
+                }
+
+            }
+            else
             {
-                barre = b;
+                this.emplacement = b.emplacement;
             }
         }
     }
